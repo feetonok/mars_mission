3c2ae434fef4548dbe2c62eea69ca7f14b3d5896
Platon <kurbatov.platon2008@gmail.com>
2025-05-05 20:17:45 +0700

    Сделал новую страницу с отображением профессий необходимых для миссии

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+PROFESSIONS = [
+    "инженер-строитель",
+    "пилот",
+    "врач",
+    "биолог",
+    "геолог",
+    "метеоролог",
+    "агроном",
+    "кибернетик",
+    "энергетик",
+    "психолог"
+]
+
 
 @app.route('/')
 @app.route('/index')
@@ -13,6 +26,13 @@ def index(title="Mars Colonization Mission"):
 @app.route('/training/<prof>')
 def training(prof):
     return render_template('training.html', prof=prof.lower())
+
+
+@app.route('/list_prof/<list_type>')
+def list_prof(list_type):
+    return render_template('list_prof.html',
+                           list_type=list_type,
+                           prof_list=PROFESSIONS)
 
 
 if __name__ == '__main__':
